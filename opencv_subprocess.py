d0baf3c15d5d162caf095d9717682393ad7200f8
kennyjin <jianianjin@gmail.com>
2021-11-14 13:22:04 -0500

    Update opencv_subprocess.py

--- opencv_subprocess.py
+++ opencv_subprocess.py
@@ -4,19 +4,21 @@ import argparse
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--input", required=True, help="path to the vidoes")
+    ap.add_argument("-i", "--input", required=True, help="path to the videos")
     ap.add_argument("-o", "--output", required=True, help="path to the output folder")
+    ap.add_argument("-py", "--python_path", default="python", help="path to python")
     args = vars(ap.parse_args())
 
     input_path = args["input"]
     output_path = args["output"]
+    python_path = args["python_path"]
 
     for file in os.listdir(input_path):
         if not file.endswith(".mp4"):
             continue
         subprocess.run(
             [
-                "python",
+                f"{python_path}",
                 f"track.py",
                 f"--prototxt=mobilenet_ssd/MobileNetSSD_deploy.prototxt",
                 "--model=mobilenet_ssd/MobileNetSSD_deploy.caffemodel",
